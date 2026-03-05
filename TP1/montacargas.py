cb6130e2aaa9d09703bed4f36d0ae60ed32b379f
Federico <fedebare04@gmail.com>
2026-03-05 12:27:27 -0300

    Montacargas.

---
+++ TP1/montacargas.py
@@ -0,0 +1,24 @@
+"""
+# Montacargas.
+El algoritmo de montacargas debe ser diseñado con el paradigma de programación orientada a objetos.
+Se le deben dar 2 parámetros:
+@param Casilla de inicio
+@param Casilla de destino
+
+El algoritmo debe retornar una lista de casillas que representan el camino a seguir para llegar al destino.
+Se debe utilizar el algoritmo de búsqueda A* para encontrar el camino más corto entre las casillas de inicio y destino,
+teniendo en cuenta las restricciones del montacargas.
+
+Se deben desarrollar las restricciones y objetivos.
+"""
+
+class Montacargas:
+    def __init__(self, 
+                 casilla_inicio: int, 
+                 casilla_destino: int):
+        self.casilla_inicio = casilla_inicio
+        self.casilla_destino = casilla_destino
+
+    def encontrar_camino(self):
+        # Implementar el algoritmo A* para encontrar el camino más corto
+        pass
