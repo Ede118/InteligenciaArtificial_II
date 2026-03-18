f663cb49f2ec06942f94dc1d894327289b00c694
JuliAvila <julieta.avila@uncuyo.edu.ar>
2026-03-18 20:38:17 -0300

    Ej 4

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -9,7 +9,7 @@ class AlgoritmoGenetico:
 
     def __init__(self):
         self.grilla = csv_to_array("TP1/utilities/casillas.csv")
-        self.N_poblacion = 50
+        self.N_poblacion = 100
         self.long_individuo = 48
         self.generaciones = 1000
         self.n_elite = 2
@@ -236,7 +236,7 @@ if __name__ == "__main__":
     ag = AlgoritmoGenetico()
 
     resultados = ag.algoritmo(
-        max_generaciones=5,
+        max_generaciones=100,
         prob_cruce=0.8,
         prob_mutacion=0.05
     )
